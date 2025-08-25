0d4178b8bafce8dfabf2aee1c1cf1894561e0229
Shaz2518 <shaziyashaikh2518@outlook.com>
2025-08-25 13:48:20 +0530

    Count-EvenOddDigits

---
+++ Java_Programs_Practice/src/NumberBased_Programs/Count_EvenOddDigits.java
@@ -0,0 +1,38 @@
+package NumberBased_Programs;
+
+import java.util.Scanner;
+
+public class Count_EvenOddDigits {
+
+	public static void main(String[] args) {
+		// Take a number from a user
+		System.out.println("Enter a number");
+		Scanner scr = new Scanner(System.in);
+		int num = scr.nextInt();
+
+		// Call Method to count digit
+		getDigitCount(num);
+
+	}
+
+	// Method for counting digits
+	public static void getDigitCount(int number) {
+		int evenCount = 0;
+		int oddCount = 0;
+		while (number != 0) {      // Loop runs until number is zero
+			
+			int num = number % 10; //Store remainder of a number in new variable
+			if (num % 2 == 0) {	  // Divide number 2 if rem =0 (even) 
+				evenCount++;
+			} else {
+				oddCount++;
+			}
+			number = number / 10; // Remove last digit from number
+		}
+		
+		// Print count
+		System.out.println("Even Count: " + evenCount + " " + "Odd Count: " + oddCount + " ");
+
+	}
+
+}
