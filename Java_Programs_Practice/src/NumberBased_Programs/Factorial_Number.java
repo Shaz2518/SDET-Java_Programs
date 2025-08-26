8a2bbdd11bbbc4b6ab172bd632f1ed36060db723
Shaz2518 <shaziyashaikh2518@outlook.com>
2025-08-26 22:44:25 +0530

    Factorial

---
+++ Java_Programs_Practice/src/NumberBased_Programs/Factorial_Number.java
@@ -0,0 +1,21 @@
+package NumberBased_Programs;
+
+import java.util.Scanner;
+
+public class Factorial_Number {
+
+	public static void main(String[] args) {
+		System.out.println("Enter a number");
+		Scanner scr = new Scanner(System.in);
+		int number=scr.nextInt();
+		int fact =1;
+		for(int i=1; i<=number; i++)
+		{
+			fact = fact*i;
+		}
+		
+		System.out.println("Factorial: " + fact);
+
+	}
+
+}
