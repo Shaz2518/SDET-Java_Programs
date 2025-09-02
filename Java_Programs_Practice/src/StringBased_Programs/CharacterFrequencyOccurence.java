6d262b70768c6d9b345745bb1bba93e761681268
Shaz2518 <shaziyashaikh2518@outlook.com>
2025-09-02 22:08:39 +0530

    Reverse a String remove spaces

---
+++ Java_Programs_Practice/src/StringBased_Programs/CharacterFrequencyOccurence.java
@@ -0,0 +1,14 @@
+package StringBased_Programs;
+
+import java.util.Scanner;
+
+public class CharacterFrequencyOccurence {
+
+	public static void main(String[] args) {
+		
+		
+		
+
+	}
+
+}
---
+++ Java_Programs_Practice/src/StringBased_Programs/ReverseString_RemoveSpace.java
@@ -0,0 +1,29 @@
+package StringBased_Programs;
+
+import java.util.Scanner;
+
+public class ReverseString_RemoveSpace {
+
+	public static void main(String[] args) {
+		Scanner scr = new Scanner(System.in);
+		System.out.println("Enter a String: ");
+		String input = scr.nextLine();
+		
+		//Remove Space from String
+		System.out.println("String Before space is: " + input);
+		String replace = input.replace(" ", "");
+		System.out.println("String without space is: " + replace);
+		
+		
+		String rev = "";
+		for(int i=replace.length()-1; i>=0; i--)
+		{
+			rev = rev + replace.charAt(i);
+		}
+		System.out.println("Reverse String is: " + rev);
+		
+		
+
+	}
+
+}
