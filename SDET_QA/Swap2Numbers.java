919f90a15ddc1509f20324326a196cbd2a81cbba
Shaz2518 <shaziyashaikh2518@outlook.com>
2025-09-12 23:25:42 +0530

    Swap

---
+++ SDET_QA/Swap2Numbers.java
@@ -0,0 +1,20 @@
+package SDET_QA;
+
+public class Swap2Numbers {
+
+	public static void main(String[] args) {
+		int num1 = 7;
+		int num2= 5;
+		int temp=0;
+		
+		//Swap
+		temp = num1;
+		num1 = num2;
+		num2 = temp;
+		
+		System.out.println("Swap: " + num1 + " " + num2);
+		
+
+	}
+
+}
