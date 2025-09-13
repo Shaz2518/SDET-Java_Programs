ca4867299d101d22f0ea348415600655038ffa50
Shaz2518 <shaziyashaikh2518@outlook.com>
2025-09-13 22:09:11 +0530

    Sum of Array

---
+++ SDET_QA/Sum_Of_Array.java
@@ -0,0 +1,17 @@
+package SDET_QA;
+
+public class Sum_Of_Array {
+
+	public static void main(String[] args) {
+		int[] arr = {5,7,1,4,0};
+		int sum = 0;
+		
+		for(int i=0; i<arr.length;i++)
+		{
+			sum += arr[i];
+		}
+
+		System.out.println("Sum of Array: " + count);
+	}
+
+}
